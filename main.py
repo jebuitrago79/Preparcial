43e8ebea7ad57d4f37972fe0a7a5dd7ca61eb690
julian <jebuitrago79@ucatolica.edu.co>
2025-04-28 22:57:28 -0500

    Se agrega el enpoint de agregar tarea

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
+from fastapi.params import Depends
 
+from db_conexiones import get_session
+from sqlmodel.ext.asyncio.session import AsyncSession
+from models import tarea
 app = FastAPI()
 
 
@@ -11,3 +15,10 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.post("/tareas")
+async def crear_nueva_tarea(tarea_data: tarea, session:AsyncSession = Depends(get_session)):
+    session.add(tarea_data)
+    await session.commit()
+    await session.refresh(tarea_data)
+    return tarea_data
